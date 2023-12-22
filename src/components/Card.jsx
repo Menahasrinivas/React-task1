28930a06dd25956049c0ac08251fdf311ddb4ba9
Menahasrinivas <menahasrinivas@gmail.com>
2023-12-22 16:18:14 +0530

    updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 function Card({data}) {
-  return <div className="col-lg-4 col-sm-6 mb-4" key={i}>
+  return <div className="col-lg-4">
       
 
   
-  <div className="data mb-5 mb-lg-0">
-    <div className="data-body">
-      <h5 className="data-title text-muted text-uppercase text-center">{data.plan}</h5>
-      <h6 className="data-price text-center">${data.price}<span className="period">/month</span></h6>
+  <div className="card mb-5 mb-lg-0">
+    <div className="card-body">
+      <h5 className="card-title text-muted text-uppercase text-center">{data.plan}</h5>
+      <h6 className="card-price text-center">${data.price}<span className="period">/month</span></h6>
       <hr/>
       <ul className="fa-ul">
         <li className={data.isUser?"":"text-muted"}><span className="fa-li"><i className={data.isUser?"fas fa-check":"fas fa-times"}></i></span>{data.plan!=="FREE"?<b>{data.user}</b>:data.user}</li>
